1f602beffa38500420d7d4d58970c87a81f0300c
EC2 Default User <ec2-user@ip-172-31-4-41.ec2.internal>
2022-03-07 03:06:52 +0000

    [Finish]bookers2-5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,37 @@
 class SearchesController < ApplicationController
-  before_action :authenticate_user!
+  def search
+    @model=params["model"]
+    @method=params["method"]
+    @content=params["content"]
+    @records=search_for(@model, @content, @method)
+  end
 
-	def search
-		@model = params[:model]
-		@content = params[:content]
-		@method = params[:method]
-		if @model == 'user'
-			@records = User.search_for(@content, @method)
-		else
-			@records = Book.search_for(@content, @method)
-		end
-	end
+  private
+  def search_for(model, content, method)
+
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == 'prefix'
+        User.where('name LIKE ?', content+'%')
+      elsif method == 'backward'
+        User.where('name LIKE ?', '%'+content)
+      else
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      elsif method == 'prefix'
+        User.where('title LIKE ?', content+'%')
+      elsif method == 'backward'
+        User.where('title LIKE ?', '%'+content)
+      else
+        Book.where('title LIKE ?', '%'+content+'%')
+      end
+    end
+
+  end
+
 end
